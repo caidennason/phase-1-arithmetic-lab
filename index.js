112fccd86f1b4ece012fc13573e369bf87068751
caiden nason <caidenbnason@gmail.com>
2022-04-13 22:20:34 -0400

    commit tree 9a6b9cdca7632d0f052a46caeca390853d3a25bb

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2
+multiply
 
+
+let random;
+random = Math.ceil(Math.random())
+random
+
+let mod;
+let num3 = 22;
+let num4 = 18;
+mod = num3 % num4;
+mod
+
+let max;
+max = Math.max(1, 20)
+max
 // Write your code here
